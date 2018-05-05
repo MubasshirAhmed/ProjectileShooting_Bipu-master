9f8bb2de6307d7b81f7fe8a1d1e9cf918a5e92ab
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-05-05 10:18:09 +0600

    initial commit

---
+++ ProjectileShooting/Assets/Scripts/ProjectileArc.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public class ProjectileArc : MonoBehaviour
+{
+    [SerializeField]
+    int iterations = 20;
+
+    [SerializeField]
+    Color errorColor;
+
+    [SerializeField]
+    Cursor targetCursor;
+
+    [SerializeField]
+    Transform firePoint;
+
+    private float currentSpeed;
+    private float currentAngle;
+
+    private Color initialColor;
+    private LineRenderer lineRenderer;
+
+    [SerializeField]
+    Transform cannonBase;
+
+    [SerializeField]
+    Transform turret;
+
+    [SerializeField]
+    GameObject projectilePrefab;
+
+    [SerializeField]
+    float cooldown = 1;
+
+    private float lastShotTime;
+
+    public int cannonAngle = 45;
+
+    Vector3 direction;
+
+    
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        initialColor = lineRenderer.material.color;
+    }
+
+    private void Update()
+    {
+        SetTargetWithAngle(targetCursor.transform.position, cannonAngle);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Fire();
+    }
+
+    public void SetTargetWithAngle(Vector3 point, float angle)
+    {
+        currentAngle = angle;
+        //point = new Vector3(-8, 0, 8);
+        direction = point - firePoint.position;
+        float yOffset = -direction.y;
+        direction = Math3d.ProjectVectorOnPlane(Vector3.up, direction);
+        float distance = direction.magnitude;
+
+        currentSpeed = ProjectileMath.LaunchSpeed(distance, yOffset, Physics.gravity.magnitude, angle * Mathf.Deg2Rad);
+
+        UpdateArc(currentSpeed, distance, Physics.gravity.magnitude, currentAngle * Mathf.Deg2Rad, direction, true);
+        SetTurret(direction, currentAngle);
+    }
+
+    public void Fire()
+    {
+        if (Time.time > lastShotTime + cooldown)
+        {
+            GameObject p = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+
+            p.GetComponent<Rigidbody>().velocity = turret.up * currentSpeed;
+            Debug.Log(currentSpeed);
+            Debug.Log(turret.up);
+            Debug.Log(p.GetComponent<Rigidbody>().velocity);
+            lastShotTime = Time.time;
+        }
+    }
+
+    private void SetTurret(Vector3 planarDirection, float turretAngle)
+    {
+        cannonBase.rotation = Quaternion.LookRotation(planarDirection) * Quaternion.Euler(-90, -90, 0);
+        turret.localRotation = Quaternion.Euler(90, 90, 0) * Quaternion.AngleAxis(turretAngle, Vector3.forward);
+    }
+
+    public void UpdateArc(float speed, float distance, float gravity, float angle, Vector3 direction, bool valid)
+    {
+        Vector2[] arcPoints = ProjectileMath.ProjectileArcPoints(iterations, speed, distance, gravity, angle);
+        Vector3[] points3d = new Vector3[arcPoints.Length];
+
+        for (int i = 0; i < arcPoints.Length; i++)
+        {
+            points3d[i] = new Vector3(0, arcPoints[i].y, arcPoints[i].x);
+        }
+
+        lineRenderer.positionCount = arcPoints.Length;
+        lineRenderer.SetPositions(points3d);
+
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        lineRenderer.material.color = valid ? initialColor : errorColor;
+    }
+}
---
+++ ProjectileShooting/Assets/Scripts/ProjectileMath.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public static class ProjectileMath
+{
+    /// <summary>
+    /// Calculates the initial launch speed required to hit a target at distance with elevation yOffset.
+    /// </summary>
+    /// <param name="distance">Planar distance from origin to the target</param>
+    /// <param name="yOffset">Elevation of the origin with respect to the target</param>
+    /// <param name="gravity">Downward acceleration in m/s^2</param>
+    /// <param name="angle">Initial launch angle in radians</param>
+    /// <returns>Initial launch speed</returns>
+    public static float LaunchSpeed(float distance, float yOffset, float gravity, float angle)
+    {
+        float speed = (distance * Mathf.Sqrt(gravity) * Mathf.Sqrt(1 / Mathf.Cos(angle))) / Mathf.Sqrt(2 * distance * Mathf.Sin(angle) + 2 * yOffset * Mathf.Cos(angle));
+
+        return speed;
+    }
+
+    /// <summary>
+    /// Samples a series of points along a projectile arc
+    /// </summary>
+    /// <param name="iterations">Number of points to sample</param>
+    /// <param name="speed">Initial speed of the projectile</param>
+    /// <param name="distance">Distance the projectile will travel along the horizontal axis</param>
+    /// <param name="gravity">Downward acceleration in m/s^2</param>
+    /// <param name="angle">Initial launch angle in radians</param>
+    /// <returns>Array of sampled points with the length of the supplied iterations</returns>
+    public static Vector2[] ProjectileArcPoints(int iterations, float speed, float distance, float gravity, float angle)
+    {
+        float iterationSize = distance / iterations;
+
+        float radians = angle;
+
+        Vector2[] points = new Vector2[iterations + 1];
+
+        for (int i = 0; i <= iterations; i++)
+        {
+            float x = iterationSize * i;
+            float t = x / (speed * Mathf.Cos(radians));
+            float y = -0.5f * gravity * (t * t) + speed * Mathf.Sin(radians) * t;
+
+            Vector2 p = new Vector2(x, y);
+
+            points[i] = p;
+        }
+
+        return points;
+    }
+}
